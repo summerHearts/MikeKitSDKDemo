a9632c7ebce823a1e75dac56f32d5dc04e274287
kenvin <summerHearts@163.com>
2017-01-13 14:39:38 +0800

    add miss projectFiles

---
+++ MikeKitFramework/MikeKitFramework.h
@@ -0,0 +1,19 @@
+//
+//  MikeKitFramework.h
+//  MikeKitFramework
+//
+//  Created by Kenvin on 17/1/13.
+//  Copyright © 2017年 上海方创金融信息服务股份有限公司. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MikeKitFramework.
+FOUNDATION_EXPORT double MikeKitFrameworkVersionNumber;
+
+//! Project version string for MikeKitFramework.
+FOUNDATION_EXPORT const unsigned char MikeKitFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MikeKitFramework/PublicHeader.h>
+
+
---
+++ MikeKitFramework/MikeKitFramework/MikeSDKInfo.h
@@ -0,0 +1,39 @@
+//
+//  MikeSDKInfo.h
+//  MikeKitLib
+//
+//  Created by Kenvin on 17/1/13.
+//  Copyright © 2017年 上海方创金融信息服务股份有限公司. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/*! SDK版本号，用于展示  SDK 的版本信息 */
+#define SDK_VERSION @"1.0.0"
+
+/*! SDK 构建ID 每次构建都会增加 */
+
+#define SDK_BUILD  1000
+@interface MikeSDKInfo : NSObject
+
+/**
+ *  DocumentDirectory
+ *
+ *  @return DocumentDirectory目录
+ */
++ (NSString *)getDocumentDirectory;
+
+/**
+ *  LibraryDirectory
+ *
+ *  @return LibraryDirectory目录
+ */
++ (NSString *)getLibraryDirectory;
+
+/**
+ *  CacheDirectory
+ *
+ *  @return CacheDirectory目录
+ */
++ (NSString *)getCacheDirectory;
+@end
